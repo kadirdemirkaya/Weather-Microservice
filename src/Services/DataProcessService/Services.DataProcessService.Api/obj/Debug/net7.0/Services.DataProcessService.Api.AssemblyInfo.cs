6d7b34af962414f6fb2d31f13ec3f2743fcc3764
AbdulKadir Demirkaya <126807887+kadirdemirkaya@users.noreply.github.com>
2024-02-19 23:21:06 +0300

    servis and ignore changed

--- src/Services/DataProcessService/Services.DataProcessService.Api/obj/Debug/net7.0/Services.DataProcessService.Api.AssemblyInfo.cs
+++ src/Services/DataProcessService/Services.DataProcessService.Api/obj/Debug/net7.0/Services.DataProcessService.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services.DataProcessService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d8c8f3d64c63b6306fb4e227df0ee933fb11ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a30720bc78306fdcfa3873ed6d75159d65ffc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services.DataProcessService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services.DataProcessService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
