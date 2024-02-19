fafc14c3e334533dc082c85235e18e986cc1b9c7
AbdulKadir Demirkaya <126807887+kadirdemirkaya@users.noreply.github.com>
2024-02-19 23:23:03 +0300

    service and building block changed

--- src/Services/LocationService/Services.LocationService/obj/Debug/net7.0/Services.LocationService.AssemblyInfo.cs
+++ src/Services/LocationService/Services.LocationService/obj/Debug/net7.0/Services.LocationService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services.LocationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d8c8f3d64c63b6306fb4e227df0ee933fb11ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a30720bc78306fdcfa3873ed6d75159d65ffc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services.LocationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services.LocationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
